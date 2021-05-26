7db41678344310c72069b63134f980b8e3fc5627
Andrew Tellitocci <antellitocci@gmail.com>
2021-05-26 15:56:25 -0400

    partial refactor to avoid issues with the DataTable when a fiat currency is changed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,4 @@
+//open dropdown fiat selection when user clicks
 var dropdown = document.querySelector('.dropdown');
 dropdown.addEventListener('click', function(event) {
   event.stopPropagation();
@@ -7,14 +8,81 @@ dropdown.addEventListener('click', function(event) {
 //create a variable to hold a user's base fiat currency selection
 var fiatSelection = JSON.parse(localStorage.getItem("baseFiat")) || "USD";
 
+//create variables for crypto data
+var cryptoImg;
+var cryptoName;
+var cryptoTicker;
+var crypto1D;
+var crypto7D;
+var cryptoCurrentPriceBase;
+var cryptoCurrentPriceUSD;
+var cryptoCurrentPriceEUR;
+var cryptoCurrentPriceCNY;
+var cryptoCurrentPriceKRW;
+var cryptoCurrentPriceJPY;
+var cryptoCurrentPriceCAD;
+var cryptoCurrentPriceGBP;
+var cryptoCurrentPriceRUB;
+var dynamicDataTable;
+
+
+function createPageElements(i){
+    //create ticker items
+    var tickerItemElem = $("<div>").addClass("ticker-item");
+    var tickerLogoElem = $("<span>").html('<img src="' + cryptoImg +'" style="height: 15px; width: 15px;"/>')
+    var tickerCryptTick = $("<span>").html(" " + cryptoTicker.toUpperCase());
+    var tickerCryptPerf = $("<span>").html(" " + parseFloat(crypto1D.toLocaleString()).toFixed(2) + "%");
+    //append ticker items to ticker tape
+    tickerItemElem.append(tickerLogoElem);
+    tickerItemElem.append(tickerCryptTick);
+    tickerItemElem.append(tickerCryptPerf);
+
+    $("#ticker-tape").append(tickerItemElem);
+
+    //create table row
+    var tableRowElem = $("<tr>");
+    //create table data elements
+    var tableFavElem = $("<td>").html('<i class="far fa-crown"></i>');
+    var tableIDElem = $("<td>").html("<b>"+ (i+1) + "</b>");
+    var tableCoinElem = $("<td>").html('<img src="' + cryptoImg +'" style="height: auto; width: 25px; float: left;"><span style="padding-left: 25px; font-size: .95em;"> <b>' + cryptoName +'</b></span>');
+    var tableCoinTicker = $("<td>").html(cryptoTicker.toUpperCase());
+    var tableCoinPriceBase = $("<td>").html("$" + cryptoCurrentPrice.toLocaleString());
+    var tableCoinPriceEUR = $("<td>").html("150");
+    var tableCoinPriceGBP = $("<td>").html("150");
+    var tableCoinPriceJPY = $("<td>").html("150");
+    var tableCoinPriceKRW = $("<td>").html("150");
+    var tableCoin1D = $("<td>").html(parseFloat(crypto1D.toLocaleString()).toFixed(2) + "%");
+    var tableCoin7D = $("<td>").html(parseFloat(crypto7D.toLocaleString()).toFixed(2) + "%");
+
+    //append elements in the correct order
+    tableRowElem.append(tableFavElem);
+    tableRowElem.append(tableIDElem);
+    tableRowElem.append(tableCoinElem);
+    tableRowElem.append(tableCoinTicker);
+    tableRowElem.append(tableCoinPriceBase);
+    tableRowElem.append(tableCoinPriceEUR);
+    tableRowElem.append(tableCoinPriceGBP);
+    tableRowElem.append(tableCoinPriceJPY);
+    tableRowElem.append(tableCoinPriceKRW);
+    tableRowElem.append(tableCoin1D);
+    tableRowElem.append(tableCoin7D);
+
+    //get reference to crypto table section and append row
+    $("#crypto-table-rows").append(tableRowElem);
+  
+}
+
 
 function getCryptocurrenciesTable(baseFiat){
-
+  if(dynamicDataTable !=undefined)
+  {
+    dynamicDataTable.destroy();
+  }
   //clear any html out so it doesn't duplicate rows
   $("#crypto-table-rows").empty();
 
   //get api URL (will need to adjust currency=X to match base selected by user)
-  var apiURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + baseFiat + "&order=market_cap_desc&per_page=150&page=1&sparkline=true&price_change_percentage=24h%2C7d%2C30d%2C1y"
+  var apiURL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" + baseFiat + "&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=24h%2C7d%2C30d%2C1y"
 
   //make request to URL
   fetch(apiURL).then(function(response){
@@ -26,75 +94,29 @@ function getCryptocurrenciesTable(baseFiat){
         for(var i = 0; i < data.length; i++)
         {
           //grab cryptocurrency information
-          var cryptoImg = data[i].image;
-          var cryptoName = data[i].name;
-          var cryptoTicker = data[i].symbol;
-          var cryptoCurrentPrice = data[i].current_price;
-          var cryptoEURPrice = "150";
-          var cryptoGBPPrice = "150";
-          var cryptoJPYPrice = "150";
-          var cryptoKRMPrice = "150";
-          var crypto1D = data[i].price_change_percentage_24h_in_currency;
-          var crypto7D = data[i].price_change_percentage_7d_in_currency;
-          // var crypto30D = data[i].price_change_percentage_30d_in_currency;
-          // var crypto1Y = data[i].price_change_percentage_1y_in_currency;
-          // var cryptoChart = data[i].sparkline_in_7d;
+           cryptoImg = data[i].image;
+           cryptoName = data[i].name;
+           cryptoTicker = data[i].symbol;
+           cryptoCurrentPrice = data[i].current_price;
 
-
-          //create ticker items
-          var tickerItemElem = $("<div>").addClass("ticker-item");
-          var tickerLogoElem = $("<span>").html('<img src="' + cryptoImg +'" style="height: 15px; width: 15px;"/>')
-          var tickerCryptTick = $("<span>").html(" " + cryptoTicker.toUpperCase());
-          var tickerCryptPerf = $("<span>").html(" " + parseFloat(crypto1D.toLocaleString()).toFixed(2) + "%");
-
-          //append ticker items to ticker tape
-          tickerItemElem.append(tickerLogoElem);
-          tickerItemElem.append(tickerCryptTick);
-          tickerItemElem.append(tickerCryptPerf);
-
-          $("#ticker-tape").append(tickerItemElem);
-
-
-          //create table row
-          var tableRowElem = $("<tr>");
-          //create table data elements
-          var tableFavElem = $("<td>").html('<i class="far fa-crown"></i>');
-          var tableIDElem = $("<td>").html("<b>"+ (i+1) + "</b>");
-          var tableCoinElem = $("<td>").html('<img src="' + cryptoImg +'" style="height: auto; width: 25px; float: left;"><span style="padding-left: 25px; font-size: .95em;"> <b>' + cryptoName +'</b></span>');
-          var tableCoinTicker = $("<td>").html(cryptoTicker.toUpperCase());
-          var tableCoinPriceBase = $("<td>").html("$" + cryptoCurrentPrice.toLocaleString());
-          var tableCoinPriceEUR = $("<td>").attr("data-eur-id", i).addClass("table-price-item").html(cryptoEURPrice);
-          var tableCoinPriceGBP = $("<td>").html(cryptoGBPPrice);
-          var tableCoinPriceJPY = $("<td>").html(cryptoJPYPrice);
-          var tableCoinPriceKRW = $("<td>").html(cryptoKRMPrice);
-          var tableCoin1D = $("<td>").html(parseFloat(crypto1D.toLocaleString()).toFixed(2) + "%");
-          var tableCoin7D = $("<td>").html(parseFloat(crypto7D.toLocaleString()).toFixed(2) + "%");
-          //var tableCoin30D = $("<td>").html(crypto30D.toFixed(2).toLocaleString());
-          //var tableCoin1Y = $("<td>").html(crypto1Y.toFixed(2).toLocaleString());
-          //var tableCoinChart = $("<td>").html(cryptoChart);
-
-          //append elements in the correct order
-          tableRowElem.append(tableFavElem);
-          tableRowElem.append(tableIDElem);
-          tableRowElem.append(tableCoinElem);
-          tableRowElem.append(tableCoinTicker);
-          tableRowElem.append(tableCoinPriceBase);
-          tableRowElem.append(tableCoinPriceEUR);
-          tableRowElem.append(tableCoinPriceGBP);
-          tableRowElem.append(tableCoinPriceJPY);
-          tableRowElem.append(tableCoinPriceKRW);
-          tableRowElem.append(tableCoin1D);
-          tableRowElem.append(tableCoin7D);
-          //tableRowElem.append(tableCoin30D);
-          //tableRowElem.append(tableCoin1Y);
-          //tableRowElem.append(tableCoinChart);
-
-          //get reference to crypto table section and append row
-          $("#crypto-table-rows").append(tableRowElem);
-          getCryptocurrencyPricesInFiat();
+          var coinToFiatURL = "https://api.coingecko.com/api/v3/coins/" + data[i].id +"?localization=false&tickers=true&market_data=true&community_data=false&developer_data=false&sparkline=false"
+          fetch(coinToFiatURL).then(function(response){
+            if(response.ok){
+              response.json().then(function(coinInfo){
+                cryptoEURPrice = coinInfo.market_data.current_price["eur"];
+                console.log(cryptoEURPrice);
+                cryptoGBPPrice = coinInfo.market_data.current_price["gbp"];
+                cryptoJPYPrice = coinInfo.market_data.current_price["jpy"];
+                cryptoKRMPrice = coinInfo.market_data.current_price["krm"];
+              });
+            }
+          });
+          
+           crypto1D = data[i].price_change_percentage_24h_in_currency;
+           crypto7D = data[i].price_change_percentage_7d_in_currency;
+           createPageElements(i);
         }
-        //Create sortable, searchable table using data table framework after the table has been created on the page
-        dynamicTableElem = $("#crypto-table").DataTable();
+        dynamicDataTable = $("#crypto-table").DataTable();
       });
 
     }
@@ -105,6 +127,7 @@ function getCryptocurrenciesTable(baseFiat){
   });
 
 };
+
 
 function getCurrencyExchangeData(baseFiat){
 
@@ -173,6 +196,7 @@ function performTableFiatConversions(base, euro, gbp, yen, won){
 
 //Get user fiat selection
 $("#dropdown-menu").on("click", function(event){
+  event.preventDefault();
   console.log("fired");
   fiatSelection = event.target.getAttribute("data-fiat");
   
@@ -182,7 +206,7 @@ $("#dropdown-menu").on("click", function(event){
     $("#base-fiat-select").text("");
     $("#base-fiat-select").html(fiatSelection.toUpperCase() + ' <i class="fas fa-caret-down"></i>');
     //pass fiat selection to the currency exchange api
-    getCurrencyExchangeData(fiatSelection);
+    // getCurrencyExchangeData(fiatSelection);
     getCryptocurrenciesTable(fiatSelection);
     //save fiat selection to local storage for page reload
   }
